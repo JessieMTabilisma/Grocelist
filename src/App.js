7baf58e2ba890243148dbed9a7c414c4a3c5f0fd
JessieMTabilisma <jessiemtabilisma@gmail.com>
2020-08-21 21:07:48 +0800

    finished update and delete reducer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react'
 import './App.css'
 import AddItem from './containers/AddItem'
 import ItemList from './containers/ItemList'
+import { Row, Col } from 'antd'
 
 function App () {
   return (
     <div className="App">
-      <h1>Grocelist</h1>
-      <AddItem />
-      <ItemList />
+      <Row gutter={[8, 32]}>
+        <Col xs={24}>
+          <h1>Grocelist</h1>
+        </Col>
+        <Col xs={24}>
+          <AddItem />
+        </Col>
+        <Col xs={24}>
+          <ItemList />
+        </Col>
+      </Row>
     </div>
   )
 }
