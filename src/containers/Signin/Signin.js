ee4d94dd8df704b61aaf20a6b44196ec3eb9e0cd
JessieMTabilisma <jessiemtabilisma@gmail.com>
2021-01-19 08:47:15 +0800

    Added illustration on Login

--- src/containers/Signin/Signin.js
+++ src/containers/Signin/Signin.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Row, Form, Input, Button, Col, Checkbox } from 'antd'
+import { Row, Form, Input, Button, Col, Checkbox, Typography } from 'antd'
 import { Link, useHistory } from 'react-router-dom'
 import Logo from '../../assets/logo.svg'
 import styles from './Signin.module.css'
 import { signin } from '../../services/auth'
 import { connect } from 'react-redux'
+import Commerce from '../../assets/commerce.svg'
 
 const Signin = props => {
   const history = useHistory()
@@ -19,11 +20,17 @@ const Signin = props => {
   return (
     <div className={styles.login}>
       <Row>
-        <img src={Logo} alt="Grocelist" className={styles.logo__login} />
+        <Col xs={24} style={{ padding: '1rem', display: 'flex' }}>
+          <img src={Logo} alt="Grocelist" className={styles.logo__login} />
+          <Typography className={styles.logo__text}>Grocelist</Typography>
+        </Col>
       </Row>
-      <Form name="login" initialValues={{ remember: true }} onFinish={onFinish} onFinishFailed={onFinishFailed}>
-        <Row>
-          <Col xs={24}>
+      <Row>
+        <Col xs={14}>
+          <img src={Commerce} alt="Commerce Login" className={styles.cover} />
+        </Col>
+        <Col xs={8} className={styles.login_form}>
+          <Form name="login" initialValues={{ remember: true }} onFinish={onFinish} onFinishFailed={onFinishFailed} layout="vertical">
             <Form.Item
               name="email"
               label="Email address"
@@ -33,12 +40,9 @@ const Signin = props => {
                   message: 'Please input your email address'
                 }
               ]}>
-              <Input size="large"/>
+
+              <Input size="large" className={styles.input__from}/>
             </Form.Item>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={24}>
             <Form.Item
               label="Password"
               name="password"
@@ -49,36 +53,24 @@ const Signin = props => {
                 }
               ]}
             >
-              <Input.Password size="large"/>
+              <Input.Password size="large" className={styles.input__from}/>
             </Form.Item>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={24}>
             <Form.Item name="remember" valuePropName="checked">
               <Checkbox>Remember me</Checkbox>
             </Form.Item>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={24}>
             <Form.Item>
-              <Button size="large" className={styles.submit__button} type="primary" htmlType="submit">
+              <Button size="large" className={styles.submit__button} type="primary" htmlType="submit" block>
                 Log in
               </Button>
             </Form.Item>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={24}>
             <Link to="/create_account">
               <Button size="large" className={styles.create__acc} type="default">
               Create account
               </Button>
             </Link>
-          </Col>
-        </Row>
-      </Form>
+          </Form>
+        </Col>
+      </Row>
     </div>
   )
 }
--- src/reducers/sample.js
+++ src/reducers/sample.js
@@ -1,74 +1,53 @@
-import { v1 as uuid } from 'uuid'
-
 const SampleProduct = [
   {
-    id: uuid(),
+
     product_name: 'Baguio Oil',
     product_image: 'https://i.ibb.co/98CVXyH/baguio-oil.jpg',
-    quantity: 1,
     price: 25
   },
   {
-    id: uuid(),
     product_name: 'Datu Puti',
     product_image: 'https://i.ibb.co/QFJjdhb/datu-puti.jpg',
-    quantity: 1,
     price: 15
   },
   {
-    id: uuid(),
     product_name: 'Twix',
     product_image: 'https://i.ibb.co/y6hqYGT/twix.jpg',
-    quantity: 1,
     price: 35
   },
   {
-    id: uuid(),
     product_name: 'Nestle milk',
     product_image: 'https://i.ibb.co/rwn1q4x/nestle-milk.jpg',
-    quantity: 1,
     price: 45
   },
   {
-    id: uuid(),
     product_name: 'Anchor butter',
     product_image: 'https://i.ibb.co/25y1yRz/butter.jpg',
-    quantity: 1,
     price: 45
   },
   {
-    id: uuid(),
     product_name: 'Dutchmill',
     product_image: 'https://i.ibb.co/pbd9Qbv/dutchmill.jpg',
-    quantity: 1,
     price: 16
   },
   {
-    id: uuid(),
     product_name: 'Pancit Cantoon',
     product_image: 'https://i.ibb.co/TvFg6tF/pancit-canton.jpg',
-    quantity: 1,
     price: 10
   },
   {
-    id: uuid(),
     product_name: 'Stick o',
     product_image: 'https://i.ibb.co/5jg619k/stick-o.jpg',
-    quantity: 1,
     price: 40
   },
   {
-    id: uuid(),
     product_name: 'Hansel',
     product_image: 'https://i.ibb.co/FHdMygQ/hansel.jpg',
-    quantity: 1,
     price: 6
   },
   {
-    id: uuid(),
     product_name: 'Cloud9',
     product_image: 'https://i.ibb.co/5L9hwmm/cloud9.jpg',
-    quantity: 1,
     price: 9
   }
 ]
